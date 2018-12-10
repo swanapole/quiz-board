014f63edafb0ecd36befb5c3be30a45c0682fcb1
swanapole <swanapoleshilla@yahoo.com>
2018-12-10 20:07:13 +0300

    Add all changes

--- js/scripts.js
+++ js/scripts.js
@@ -10,37 +10,56 @@ function typeWriter() {
   }
 }
 
- $(document).ready(function(){
-   $("#click1").on("click", function(){
-     $(".quiz1").toggle();
-   });
- });
 
- $(document).ready(function(){
-   $("#click2").on("click", function(){
-     $(".quiz2").toggle();
-   });
- });
-
- $(document).ready(function(){
-   $("#click3").on("click", function(){
-     $(".quiz3").toggle();
-   });
- });
-
- $(document).ready(function(){
-   $("#click4").on("click", function(){
-     $(".quiz4").toggle();
-   });
- });
 function run(){
 
-  var question1=document.quiz.question1.value;
-  var question1=document.quiz.question2.value;
-  var question1=document.quiz.question3.value;
-  var question1=document.quiz.question4.value;
-  var question1=document.quiz.question5.value;
+  var question1=parseInt('input[name="question1"]:checked').value;
+  var question2=parseInt('input[name="question2"]:checked').value;
+  var question3=parseInt('input[name="question3"]:checked').value;
+  var question4=parseInt('input[name="question4"]:checked').value;
   var correct=0;
 
-  v
+  if(question1 === "Function"){
+    correct+=25;
+  }
+  if(question2 === "JAVASCRIPT"){
+    correct+=25;
+  }
+  if(question3 === "1995"){
+    correct+=25;
+  }
+  if(question4 === "frog"!=="lizard"){
+    correct+=25;
+  }
+  alert(correct);
+
+
+   if(correct <= 25){
+     var messages="You can do better";
+      document.getElementById("#show").innerHTML=message
+   }
+   if(correct > 25 && correct < 75){
+     var messages="average work";
+     document.getElementById("#show").innerHTML=message
+
+   }
+   if(correct > 50 && correct < 100){
+     var messages="good job";
+    document.getElementById("#show").innerHTML=message
+   }
+   if(correct > 75 && correct < 110){
+     var messages="Amazing job";
+    document.getElementById("#show").innerHTML=message
+   }
+
+   $("#show").text(correct);
+   $("#button").click(function () {
+     $(".quiz1").hide();
+     $(".cover").show();
+   }
+   )
+
+  $("#number_correct").text(correct);
+  // document.getElementById("#number_correct").innerHTML="you got" + correct +"correct."
+
 }
