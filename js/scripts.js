f97e15bc759c2789141a0dcb43aa246eeb6f637a
swanapole <45202362+swanapole@users.noreply.github.com>
2018-12-11 12:26:21 +0300

    add all the changes

--- js/scripts.js
+++ js/scripts.js
@@ -11,55 +11,102 @@ function typeWriter() {
 }
 
 
-function run(){
+var inputedAnswers = [],
+    scoredPoints = 0;
+    questions = ["ans-1", "ans-2", "ans-3", "ans-4"];
 
-  var question1=parseInt('input[name="question1"]:checked').value;
-  var question2=parseInt('input[name="question2"]:checked').value;
-  var question3=parseInt('input[name="question3"]:checked').value;
-  var question4=parseInt('input[name="question4"]:checked').value;
-  var correct=0;
-
-  if(question1 === "Function"){
-    correct+=25;
-  }
-  if(question2 === "JAVASCRIPT"){
-    correct+=25;
-  }
-  if(question3 === "1995"){
-    correct+=25;
-  }
-  if(question4 === "frog"!=="lizard"){
-    correct+=25;
-  }
-  alert(correct);
-
-
-   if(correct <= 25){
-     var messages="You can do better";
-      document.getElementById("#show").innerHTML=message
-   }
-   if(correct > 25 && correct < 75){
-     var messages="average work";
-     document.getElementById("#show").innerHTML=message
-
-   }
-   if(correct > 50 && correct < 100){
-     var messages="good job";
-    document.getElementById("#show").innerHTML=message
-   }
-   if(correct > 75 && correct < 110){
-     var messages="Amazing job";
-    document.getElementById("#show").innerHTML=message
-   }
-
-   $("#show").text(correct);
-   $("#button").click(function () {
-     $(".quiz1").hide();
-     $(".cover").show();
-   }
-   )
-
-  $("#number_correct").text(correct);
-  // document.getElementById("#number_correct").innerHTML="you got" + correct +"correct."
-
+function pointsCompiler(){
+  scoredPoints = inputedAnswers[0] + inputedAnswers[1] + inputedAnswers[2] + inputedAnswers[3];
+  return points;
 }
+
+$(document).ready(function(){
+  $(".question").hide();
+  $("#q-1").show();
+
+  $("#btn-1").click(function(){
+    if(isNaN($("input:radio[name = ans-1]:checked").val())){
+      alert("Please answer question 1 to proceed!");
+      $(".question").hide();
+      $("#q-1").show();
+      return false;
+    }else{
+      $(".question").hide();
+      $("#q-1").hide();
+      $("#q-2").show();
+      return false;
+    }
+  });
+
+  $("#btn-2-1").click(function(){
+    $(".question").hide();
+    $("#q-2").hide();
+    $("#q-1").show();
+    return false;
+  });
+  $("#btn-2-2").click(function(){
+    if(isNaN($("input:radio[name = ans-2]:checked").val())){
+      alert("Please answer question 2 to proceed!");
+      $(".question").hide();
+      $("#q-1").hide();
+      $("#q-2").show();
+      return false;
+    }else{
+      $(".question").hide();
+      $("#q-2").hide();
+      $("#q-3").show();
+      return false;
+    }
+  });
+
+  $("#btn-3-1").click(function(){
+    $(".question").hide();
+    $("#q-3").hide();
+    $("#q-2").show();
+    return false;
+  });
+  $("#btn-3-2").click(function(){
+    if(isNaN($("input:radio[name = ans-3]:checked").val())){
+      alert("Please answer question 3 to proceed!");
+      $(".question").hide();
+      $("#q-2").hide();
+      $("#q-3").show();
+      return false;
+    }else{
+      $(".question").hide();
+      $("#q-3").hide();
+      $("#q-4").show();
+      return false;
+    }
+  });
+
+  $("#btn-4-1").click(function(){
+    $(".question").hide();
+    $("#q-4").hide();
+    $("#q-3").show();
+    return false;
+  });
+
+  $("#btn-4-2").click(function(){
+    if(isNaN($("input:radio[name = ans-4]:checked").val())){
+      alert("Please answer question 4 to submit your answers!");
+      $(".question").hide();
+      $("#q-3").hide();
+      $("#q-4").show();
+      return false;
+    }else{
+      $("#fm-1").submit(function(event){
+        event.preventDefault();
+        questions.forEach(function(question){
+          inputedAnswers.push(parseInt($("input:radio[name=" + question + "]:checked").val()));
+        });
+        pointsCompiler();
+
+        $("#test").hide();
+        $("#q-4").hide();
+        $("#output").show();
+        $("#points").append(scoredPoints);
+      });
+    }
+  });
+});
