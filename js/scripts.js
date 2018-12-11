8fc736b41a9a16b746268766c70a872b6d7fa059
swanapole <45202362+swanapole@users.noreply.github.com>
2018-12-11 14:01:41 +0300

    add function

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,28 @@ function typeWriter() {
   }
 }
 
+var inputedAnswers = [],
+    scoredPoints = 0;
+    questions = ["ans-1", "ans-2", "ans-3", "ans-4"];
+
+function pointsCompiler(){
+  scoredPoints = inputedAnswers[0] + inputedAnswers[1] + inputedAnswers[2] + inputedAnswers[3];
+  return points;
+}
+
+$(document).ready(function(){
+  $(".question").hide();
+  $("#q-1").show();
+
+  $("#btn-1").click(function(){
+    if(isNaN($("input:radio[name = ans-1]:checked").val())){
+      alert("Please answer question 1 to proceed!");
+      $(".question").hide();
+      $("#q-1").show();
+      return false;
+    
+
+
 
 
 
