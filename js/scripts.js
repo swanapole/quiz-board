921c1552da5e8078db4e073cfe778eb48f77c126
swanapole <45202362+swanapole@users.noreply.github.com>
2018-12-11 14:00:22 +0300

    add function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+var i = 0;
+var txt = 'Javascript quiz board .';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
 
 
 
