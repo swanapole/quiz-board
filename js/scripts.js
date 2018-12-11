5dbcf943e74a8e52fc150c977de9f065635c67b5
swanapole <45202362+swanapole@users.noreply.github.com>
2018-12-11 13:59:22 +0300

    remove some styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,4 @@
-var i = 0;
-var txt = 'Javascript quiz board .';
-var speed = 50;
 
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
 
 
  
